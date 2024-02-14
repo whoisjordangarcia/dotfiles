5eef3bccd9021164ce3ea21a1c55750da4593c7a
Jordan Garcia <arickho@gmail.com>
2024-02-13 20:41:16 -0500

    feat: symlink nvim configuration

---
+++ configs/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
