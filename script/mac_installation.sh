1132e5eadf28e411dd281a8ee49bcd11fd0a3f2c
Jordan Garcia <arickho@gmail.com>
2024-04-03 22:56:33 -0400

    moves files

--- script/mac_installation.sh
+++ script/mac_installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 source "$SCRIPT_DIR/common/log.sh"
 
@@ -9,26 +9,24 @@ source "$SCRIPT_DIR/common/log.sh"
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle --file Brewfile
 
-
 if [ ! -d "~/git" ]; then
-  mkdir ~/git
+	mkdir ~/git
 fi
 
 (cd ~/git && git clone https://github.com/dracula/iterm.git)
 
 # Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz);
-then
+if test ! $(which omz); then
 	info "omz exists!"
 else
 	info "installing oh-my-zsh..."
@@ -41,6 +39,10 @@ fi
 
 # Setup setup vim
 "$SCRIPT_DIR/vim/setup.sh"
+
+# Install tmux
+brew install tmux
+"$SCRIPT_DIR/tmux/setup.sh"
 
 # Install fzf
 brew install fzf
--- script/zsh/setup.sh
+++ script/zsh/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 source "$SCRIPT_DIR/../common/log.sh"
 
@@ -11,17 +11,17 @@ DATE_SUFFIX=$(date +%Y%m%d)
 
 ZSHRC_BACKUP_PATH="$HOME/.zshrc_backup_$DATE_SUFFIX"
 
-ZSHRC_SYMLINK_TARGET="$SCRIPT_DIR/../../configs/.zshrc"
+ZSHRC_SYMLINK_TARGET="$SCRIPT_DIR/../../configs/zshrc/.zshrc"
 
 if [ -f "$ZSHRC_PATH" ]; then
-    info "Identified .zshrc exists creating backup"
-    # Backup the file
-    cp "$ZSHRC_PATH" "$ZSHRC_BACKUP_PATH"
-    echo "Backup created at $ZSHRC_BACKUP_PATH"
+	info "Identified .zshrc exists creating backup"
+	# Backup the file
+	cp "$ZSHRC_PATH" "$ZSHRC_BACKUP_PATH"
+	echo "Backup created at $ZSHRC_BACKUP_PATH"
 
-    info "Deleting file $ZSHRC_PATH"
-    rm "$ZSHRC_PATH"
-fi    
+	info "Deleting file $ZSHRC_PATH"
+	rm "$ZSHRC_PATH"
+fi
 
 # Create a symlink if .zshrc doesn't exist
 ln -s "$ZSHRC_SYMLINK_TARGET" "$ZSHRC_PATH"
@@ -29,3 +29,4 @@ info "Symlink created for $ZSHRC_PATH"
 
 # Install zsh plugins
 "$SCRIPT_DIR/plugins/zsh_plugins.sh"
+
