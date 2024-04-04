49c048fa20286c54fa95685883ef1c87511d2ee2
Jordan Garcia <arickho@gmail.com>
2024-04-03 22:37:16 -0400

    vim installation and wezterm and tmux updates

--- configs/wezterm/windows/.wezterm.lua
+++ configs/wezterm/windows/.wezterm.lua
@@ -2,16 +2,16 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.wsl_domains = {
-	name = "WSL:Ubuntu",
-	distributions = "Ubuntu",
-}
-
-config.default_domain = "WSL:Ubuntu"
-
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
+
+config.window_padding = {
+	left = 5,
+	right = 5,
+	bottom = 0,
+	top = 0,
+}
 
 config.color_scheme = "Catppuccin Mocha"
 
