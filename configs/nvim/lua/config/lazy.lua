9cef5186b8503068c57da1b56f167b02efa19b38
Jordan Garcia <arickho@gmail.com>
2024-04-12 10:25:39 -0400

    feat: adds new nvim plugins using kickstart.nvim

--- configs/nvim/lua/config/lazy.lua
+++ configs/nvim/lua/config/lazy.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- bootstrap lazy.nvim
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
--- configs/nvim/lua/config/options.lua
+++ configs/nvim/lua/config/options.lua
@@ -2,6 +2,61 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
+-- Set <space> as the leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-opt.wrap = true
+-- Nerd Font installed
+vim.g.have_nerd_font = true
+
+-- Line numbers defautl
+vim.opt.number = true
+
+-- Useful for jumping, but i'm not there yet
+vim.opt.relativenumber = false
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Don't show, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim
+vim.opt.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive while searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Display which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- How splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Helps display certain whitespace characters in the editor
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+vim.opt.cursorline = true
+-- Show which line your cursor is on
+
+-- Minimal number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 10
