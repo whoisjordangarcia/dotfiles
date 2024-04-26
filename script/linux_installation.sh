0fb00c0a5b3dfb8119b19e8166badbf00e65b234
Jordan Garcia <arickho@gmail.com>
2024-04-25 20:19:54 -0400

    ripgrip

--- script/linux_installation.sh
+++ script/linux_installation.sh
@@ -1,19 +1,19 @@
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+info() {
+	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+user() {
+	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+success() {
+	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
+fail() {
+	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+	echo ''
+	exit
 }
 
 # Fail on any command.
@@ -59,6 +59,9 @@ apt-get install software-properties-common
 add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
 apt-get install neovim
+
+# Install ripgrep
+apt install ripgrep
 
 # Override the config for .zshrc
 info "Defaulting copy .zshrc file over..."
