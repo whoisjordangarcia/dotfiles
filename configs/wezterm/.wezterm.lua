b71ea8fa3f385972abe711130872df39f0dd3ac8
Jordan Garcia <arickho@gmail.com>
2024-05-06 10:01:43 -0400

    wezterm config updates regarding window decorations

--- configs/wezterm/.wezterm.lua
+++ configs/wezterm/.wezterm.lua
@@ -10,11 +10,14 @@ config.window_padding = {
 	left = 5,
 	right = 5,
 	bottom = 0,
-	top = 0,
+	top = 10,
 }
-config.window_decotrations = "NONE"
+config.window_decorations = "NONE"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 20
 config.color_scheme = "Catppuccin Mocha"
-
+config.window_background_opacity = 0.9
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.enable_tab_bar = false
 return config
