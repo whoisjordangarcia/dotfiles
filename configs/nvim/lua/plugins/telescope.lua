448ba2a39199ec69a0f136c30c2bc8dfb982c291
Jordan Garcia <arickho@gmail.com>
2024-05-29 14:10:09 -0400

    telescope vizual adjustments

---
+++ configs/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_strategy = "vertical",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+    },
+  },
+}
