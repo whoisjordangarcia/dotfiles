cdadb4f24579e784cf5e88e5262d2c36c1b757fb
Jordan Garcia <jordan.garcia@invitae.com>
2024-12-01 23:43:32 -0500

    Update linux_ubuntu_installation.sh

--- script/linux_ubuntu_installation.sh
+++ script/linux_ubuntu_installation.sh
@@ -23,7 +23,7 @@ sudo apt eza
 ## upgrade neovim - ubuntu has an old version of nvim by default. We want to upgrade that.
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt upgrade neovim
+sudo apt-get install neovim
 
 sudo apt install -y \
 	gcc \
