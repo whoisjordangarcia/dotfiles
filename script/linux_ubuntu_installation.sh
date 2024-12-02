fe2756ae8a3faf977a6391654a835c46532e3d89
Jordan Garcia <jordan.garcia@invitae.com>
2024-12-01 23:20:37 -0500

    Update linux_ubuntu_installation.sh

--- script/linux_ubuntu_installation.sh
+++ script/linux_ubuntu_installation.sh
@@ -4,11 +4,17 @@ source ./script/common/log.sh
 
 
 sudo apt install curl
+
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
 
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+sudo apt update
 
 sudo apt install -y \
 	gcc \
