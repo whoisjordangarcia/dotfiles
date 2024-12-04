180b66bb74fab2db26b20fb84de81eaf667661fc
Jordan Garcia <arickho@gmail.com>
2024-12-03 22:06:14 -0500

    fix tmux setup and fix linux installation setup

--- script/linux_ubuntu_installation.sh
+++ script/linux_ubuntu_installation.sh
@@ -20,6 +20,12 @@ sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.lis
 sudo apt update
 sudo apt eza
 
+## lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
 ## neovim - Use the unstable version of nvim - lazy requires >0.8
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
