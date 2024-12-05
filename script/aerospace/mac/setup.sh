747b25ea01fa6e5d1b9435bdc342a1a2eff13f55
Jordan Garcia <arickho@gmail.com>
2024-12-05 18:34:04 -0500

    adds sketchybar

--- script/aerospace/mac/setup.sh
+++ script/aerospace/mac/setup.sh
@@ -5,13 +5,23 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "$SCRIPT_DIR/../../common/log.sh"
 
 AEROSPACE_PATH="$HOME/.aerospace.toml"
-
 AEROSPACE_SYMLINK_TARGET="$SCRIPT_DIR/../../../configs/aerospace/.aerospace.toml"
+
+SKETCHYBAR_PATH="$HOME/.config/sketchybar/sketchybarrc"
+SKETCHYBAR_SYMLINK_TARGET="$SCRIPT_DIR/../../../configs/aerospace/sketchybarrc"
 
 if [ -f "$AEROSPACE_PATH" ]; then
 	info "Identified .aerospce.toml exists. please delete manually"
+else
+	# Create a symlink if .aerospace.toml doesn't exist
+	ln -s "$AEROSPACE_SYMLINK_TARGET" "$AEROSPACE_PATH"
+	info "Symlink created for $AEROSPACE_PATH"
 fi
 
-# Create a symlink if .aerospace.toml doesn't exist
-ln -s "$AEROSPACE_SYMLINK_TARGET" "$AEROSPACE_PATH"
-info "Symlink created for $AEROSPACE_PATH"
+if [ -f "$SKETCHYBAR_PATH" ]; then
+	info "Identified sketchybarrc exists. please delete manually"
+else
+	# Create a symlink if .aerospace.toml doesn't exist
+	ln -s "$SKETCHYBAR_SYMLINK_TARGET" "$SKETCHYBAR_PATH"
+	info "Symlink created for $SKETCHYBAR_SYMLINK_TARGET"
+fi
