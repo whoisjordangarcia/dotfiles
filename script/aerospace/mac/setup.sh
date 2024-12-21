54c105b4f36900c678571cc620cc50cb4445c33e
Jordan Garcia <arickho@gmail.com>
2024-12-20 21:03:06 -0500

    aerospace

--- script/aerospace/mac/setup.sh
+++ script/aerospace/mac/setup.sh
@@ -6,18 +6,20 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "$SCRIPT_DIR/../../common/log.sh"
 source "$SCRIPT_DIR/../../common/symlink.sh"
 
-AEROSPACE_SOURCE="$SCRIPT_DIR/../../../configs/aerospace/.aerospace.toml"
+AEROSPACE_SOURCE="$SCRIPT_DIR/../../configs/aerospace/.aerospace.toml"
 AEROSPACE_TARGET="$HOME/.aerospace.toml"
 
 link_file "$AEROSPACE_SOURCE" "$AEROSPACE_TARGET"
 
-SKETCHYBAR_SOURCE="$SCRIPT_DIR/../../../configs/aerospace/sketchybarrc"
+SKETCHYBAR_SOURCE="$SCRIPT_DIR/../../configs/aerospace/sketchybarrc"
 SKETCHYBAR_TARGET="$HOME/.config/sketchybar/sketchybarrc"
+
+mkdir "$HOME/.config/sketchybar"
 
 link_file "$SKETCHYBAR_SOURCE" "$SKETCHYBAR_TARGET"
 
 # allows to move windows by dragging any part of the window using Ctrl + Cmd
-run defaults write -g NSWindowShouldDragOnGesture -bool tre
+defaults write -g NSWindowShouldDragOnGesture -bool tre
 
 # disable windows opening animations
-run defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
