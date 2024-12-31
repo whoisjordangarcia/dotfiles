f22ca4e4dee43d439a2811b11554c4f2793e0adf
Jordan Garcia <arickho@gmail.com>
2024-12-31 15:48:30 -0500

    aerospace setup and sketchybar

--- script/apps/mac/setup.sh
+++ script/apps/mac/setup.sh
@@ -4,6 +4,10 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 source "$SCRIPT_DIR/../../common/log.sh"
 
+# Install xCode cli tools
+info "Install commandline tools..."
+xcode-select --install
+
 # Check if Homebrew is installed
 if ! command -v brew &>/dev/null; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,6 +15,8 @@ if ! command -v brew &>/dev/null; then
 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zprofile
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+brew analytics off
 
 # Tap Homebrew Bundle if not already tapped
 if ! brew tap | grep -q '^homebrew/bundle$'; then
@@ -20,3 +26,5 @@ fi
 
 info "Installing and upgrading apps from Brewfile..."
 brew bundle --file=Brewfile
+
+brew services start borders
