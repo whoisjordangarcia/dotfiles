e34bd6d5b769b8b40d0c6e01919fe7f6065b23c4
Jordan Garcia <arickho@gmail.com>
2025-01-02 09:53:15 -0500

    fix lua

--- configs/aerospace/sketchybar/helpers/default_font.lua
+++ configs/aerospace/sketchybar/helpers/default_font.lua
@@ -1,6 +1,6 @@
 return {
-	text = "JetBrainsMono Nerd Font", -- Used for text
-	numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+	text = "SF Pro", -- Used for text
+	numbers = "SF Mono", -- Used for numbers
 
 	-- Unified font style map
 	style_map = {
