58d6341f02dd5b31d1a5aabcdda64e1f3c2ffacf
Jordan Garcia <arickho@gmail.com>
2025-01-10 22:14:32 -0500

    made fixes

--- script/lazygit/linux/setup.sh
+++ script/lazygit/linux/setup.sh
@@ -3,10 +3,13 @@ set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-source "$SCRIPT_DIR/../common/log.sh"
-source "$SCRIPT_DIR/../common/symlink.sh"
+source "$SCRIPT_DIR/../../common/log.sh"
+source "$SCRIPT_DIR/../../common/symlink.sh"
 
 LAZYGIT_SOURCE="$SCRIPT_DIR/../../configs/lazygit/config.yml"
 LAZYGIT_TARGET="$HOME/.config/lazygit/config.yml"
+
+mkdir "$HOME/.config"
+mkdir "$HOME/.config/lazygit"
 
 link_file "$LAZYGIT_SOURCE" "$LAZYGIT_TARGET"
