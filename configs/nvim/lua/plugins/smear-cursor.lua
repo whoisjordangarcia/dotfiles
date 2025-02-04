8f7245834b7156ed14f5581ad27ecbcd5c34e224
Jordan Garcia <arickho@gmail.com>
2025-02-04 10:50:45 -0500

    fast smear cursor with transparent background support

---
+++ configs/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      legacy_computing_symbols_support = true,
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+    },
+  },
+}
