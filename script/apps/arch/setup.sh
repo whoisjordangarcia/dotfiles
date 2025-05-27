e3a4e4b96fa93d04341e0b5042a66a8de3435308
Jordan Garcia <arickho@gmail.com>
2025-05-26 22:29:16 -0400

    arch updates

--- script/apps/arch/setup.sh
+++ script/apps/arch/setup.sh
@@ -4,12 +4,20 @@ pacman -S zsh starship
 # Development Tools
 pacman -S gh neovim
 
-pacman -S tmux ripgrep eza zoxide wl-clipboard
+pacman -S tmux ripgrep eza zoxide wl-clipboard fzf jq
+
+pacman -S dysk htop
 
 # Fun
 pacman -S lolcat figlet
 
+# Photos
+pacman -S darktable
+
 # Fonts
 pacman -S ttf-jetbrains-mono-nerd
 
-yay -S cursor-bin streamdeck-ui ghostty-git
+yay -S cursor-bin ghostty-git spotify_player
+
+# Games
+pacman -S gamemode mangohud
