6bb46548e11ecafd2d092071766e027ec701f320
Jordan Garcia <arickho@gmail.com>
2025-09-16 09:31:21 -0400

    feat: adds a bootscript

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,22 +2,6 @@
 
 source "./script/common/log.sh"
 
-# Display ASCII art logo
-show_logo() {
-    local logo_file="./art/logo"
-    if [[ -f "$logo_file" ]]; then
-        echo ""
-        # Display logo in blue color
-        while IFS= read -r line; do
-            printf "${BLUE}%s${RESET}\n" "$line"
-        done < "$logo_file"
-        echo ""
-    fi
-}
-
-# Show the logo
-show_logo
-
 # Run interactive dotfiles setup
 ./bin/dot -i
 
