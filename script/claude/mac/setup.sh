d562bdd9851df621f6879e5a3a6dfc1f75aecc3f
Jordan Garcia <arickho@gmail.com>
2025-09-16 09:25:01 -0400

    feat: improvements

--- script/claude/mac/setup.sh
+++ script/claude/mac/setup.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 source "$SCRIPT_DIR/../../common/log.sh"
 source "$SCRIPT_DIR/../../common/symlink.sh"
 
-info "Ensuring $HOME/.claude exists"
+debug "Ensuring $HOME/.claude exists"
 mkdir -p "$HOME/.claude"
 
 link_file "$SCRIPT_DIR/../../configs/claude/agents/" "$HOME/.claude/agents" "directory"
--- script/work/mac/setup.sh
+++ script/work/mac/setup.sh
@@ -10,5 +10,3 @@ git config --global user.email "jordan.garcia@labcorp.com"
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 export PATH="/Users/jordan.garcia/.rd/bin:$PATH"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
-
-info "dummy work shell"
