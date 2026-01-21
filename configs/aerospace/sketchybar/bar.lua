3bfce620bbddb9d7fe5d7436e75427462d6b6cfa
Jordan Garcia <jordan.garcia@labcorp.com>
2026-01-20 19:43:16 -0500

    fix sketchybar location
    Signed-off-by: Jordan Garcia <jordan.garcia@labcorp.com>

--- configs/aerospace/sketchybar/bar.lua
+++ configs/aerospace/sketchybar/bar.lua
@@ -6,7 +6,8 @@ sbar.bar({
 	height = 40,
 	color = 0xff2c2e34, -- More opaque background
 	shadow = true,
-	y_offset = -44, -- Move above Aerospace's top gap
+	--y_offset = -44, -- Move above Aerospace's top gap
+	y_offset = 0,
 	padding_right = 15,
 	padding_left = 15,
 })
